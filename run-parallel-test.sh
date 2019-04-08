16826925664b44b932d99c36542d282fa4f613fd
Stephan Klevenz <stephan.klevenz@sap.com>
2019-04-08 17:49:39 +0200

    url support

--- run-parallel-test.sh
+++ run-parallel-test.sh
@@ -3,22 +3,23 @@
 echo "$$" > "PID_PARALLEL"
 
 if [ "$1" == "" ]; then
-  echo "Usage: run-parallel-test.sh [landscape-domain]"
-  echo "Example: run-parallel-test.sh eu10.hana.ondemand.com"
+  echo "Usage: run-parallel-test.sh [url]"
   exit 0
 fi
 
-DOMAIN="$1"
+URL="$1"
 FILE="$(mktemp)"
 
 rm -rf gen/parallel
 mkdir -p gen/parallel
 
 for i in `seq 1 50`; do
-  echo "https://x-forwarded-for.cf.$DOMAIN/" >> $FILE
-  echo "https://api.cf.$DOMAIN/v2/info/" >> $FILE
+  echo "$URL" >> $FILE
 done
 
+cat $FILE
+
 while true; do
-  cat "$FILE" | parallel "curl -sSi -X POST -d @data-1MB.file {}" >> "gen/parallel/$DOMAIN.log" 2>> "gen/parallel/$DOMAIN.err"
+  #  cat "$FILE" | parallel "curl -sSi -X POST -d @data-1MB.file {}" >> "gen/parallel/response.log" 2>> "gen/parallel/response.err"
+  cat "$FILE" | parallel "curl -sSi {}" >> "gen/parallel/response.log" 2>> "gen/parallel/response.err"
 done
